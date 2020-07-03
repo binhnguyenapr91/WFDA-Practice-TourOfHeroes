5ffe95b075cd64c628097783eca0ab745fe83f04
binhnguyenapr91 <binhnguyen.apr91@gmail.com>
2020-07-03 16:48:53 +0700

    finish

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -6,7 +6,7 @@ import {HEROES} from '../mock-heros';
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
-  styleUrls: ['./heroes.component.scss']
+
 })
 export class HeroesComponent implements OnInit {
   heroes = HEROES;
