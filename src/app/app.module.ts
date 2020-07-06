935884f7deda9cb37c264783e88aee0c2bd50c26
binhnguyenapr91 <binhnguyen.apr91@gmail.com>
2020-07-06 15:22:22 +0700

    finished

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,7 +7,7 @@ import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
 import { from } from 'rxjs';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
-import { MessagesComponent } from './messages/messages.component';
+import { MessagesComponent } from './messages/messages.component';;
 
 @NgModule({
   declarations: [
